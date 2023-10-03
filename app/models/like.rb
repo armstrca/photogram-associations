b94c881ea7753efeac60a96ba7cfa259c434f552
Calvin Armstrong <138508650+armstrca@users.noreply.github.com>
2023-10-03 22:12:09 +0000

    hahahahaha whoops

--- app/models/like.rb
+++ app/models/like.rb
@@ -24,23 +24,25 @@ class Like < ApplicationRecord
 
   # Like#photo: returns a row from the photo table associated to this like by the photo_id column
 
-  def fan
-    my_fan_id = self.fan_id
+  belongs_to(:fan, :class_name => "User", :foreign_key => "username")
+  # def fan
+  #   my_fan_id = self.fan_id
 
-    matching_users = User.where({ :id => my_fan_id })
+  #   matching_users = User.where({ :id => my_fan_id })
 
-    the_user = matching_users.at(0)
+  #   the_user = matching_users.at(0)
 
-    return the_user
-  end
+  #   return the_user
+  # end
 
-  def photo
-    my_photo_id = self.photo_id
+  belongs_to(:photo, :class_name => "Photo", :foreign_key => "photo_id")
+  # def photo
+  #   my_photo_id = self.photo_id
 
-    matching_photos = Photo.where({ :id => my_photo_id })
+  #   matching_photos = Photo.where({ :id => my_photo_id })
 
-    the_photo = matching_photos.at(0)
+  #   the_photo = matching_photos.at(0)
 
-    return the_photo
-  end
+  #   return the_photo
+  # end
 end
